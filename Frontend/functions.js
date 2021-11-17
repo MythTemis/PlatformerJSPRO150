98e8fd277e8773ac3262ade6650b41a088a01c7f
Taya Richardson_ <trichardson@student.neumont.edu>
2021-11-16 22:54:39 -0700

    Enemy Creation
    Creates a basic enemy with basic random movement

--- Frontend/functions.js
+++ Frontend/functions.js
@@ -1,9 +1,13 @@
 var canvas = document.getElementById("game");
 canvas.width = 720;
 canvas.height = 720;
+const minX = 0, maxX = 700;
+const minY = 0, maxY = 700;
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
+var enemyX = Math.random() * (maxX - minX) + minX;
+var enemyY = Math.random() * (maxY - minY) + minY;
 var dx = 0;
 var dy = 0;
 const charDimension = [10, 10, 10, 20]
@@ -18,12 +22,39 @@ const drawCharacter = () => {
     ctx.closePath();
 }
 
-const draw = () => {
+const drawEnemy = () => {
+    ctx.beginPath();
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(enemyX, enemyY, 30, 40);
+    ctx.closePath();
+}
+
+const drawUpdate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawCharacter();
+    drawEnemy();
     x += dx;
     y -= dy;
 }
+
+const drawEnemyUpdate = () => {
+    let num = Math.floor(Math.random() * (100 - 0 + 1) + 0);
+    let num2 = Math.floor(Math.random() * (100 - 0 + 1) + 0);
+
+    if((num % 2) == 0){
+        enemyX += 3;
+    } else{
+        enemyX -= 3;
+    }
+
+    if((num2 % 2) == 0){
+        enemyY += 3;
+    } else{
+        enemyY -= 3;
+    }
+}
+
+
 
 //Key down event listener
 document.addEventListener('keydown',  (evt) => {
@@ -82,7 +113,7 @@ document.addEventListener('keyup', (evt) => {
 }, false);
 
 const moveUp = (isPressed) => {
-    if(isPressed) {
+    if(isPressed && y < maxY) {
         dy = 5;
     }else{
         dy = 0;
@@ -90,7 +121,7 @@ const moveUp = (isPressed) => {
 };
 
 const moveDown = (isPressed) => {
-    if(isPressed) {
+    if(isPressed && y > minY) {
         dy = -5;
     }else{
         dy = 0;
@@ -98,7 +129,7 @@ const moveDown = (isPressed) => {
 };
 
 const moveLeft = (isPressed) => {
-    if(isPressed) {
+    if(isPressed && x > minX) {
         dx = -5;
     }else{
         dx = 0;
@@ -106,7 +137,7 @@ const moveLeft = (isPressed) => {
 };
 
 const moveRight = (isPressed) => {
-    if(isPressed) {
+    if(isPressed && x < maxX) {
         dx = 5;
     }else{
         dx = 0;
@@ -114,6 +145,7 @@ const moveRight = (isPressed) => {
 };
 
 drawCharacter();
-setInterval(draw, 10);
-
+drawEnemy();
+setInterval(drawUpdate, 10);
+setInterval(drawEnemyUpdate, 100);
 
