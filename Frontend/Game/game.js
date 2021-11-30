ef17df09e39d8c1b3594304e799601d6ac60c2ac
Taya Richardson_ <trichardson@student.neumont.edu>
2021-11-29 19:24:50 -0700

    Minimization of files

--- Frontend/Game/game.js
+++ Frontend/Game/game.js
@@ -298,8 +298,6 @@ Game.Player = function(x, y) {
 
   Game.MovingObject.call(this, x, y, 7, 12);
 
-  Game.Animator.call(this, Game.Player.prototype.frame_sets["idle-left"], 10);
-
   this.jumping     = true;
   this.direction_x = -1;
   this.velocity_x  = 0;
@@ -365,14 +363,12 @@ Game.TileSet = function(columns, tile_size) {
 
   let f = Game.Frame;
 
-  this.frames = [new f(115,  96, 13, 16, 0, -4), // idle-left
+  this.frames = [
                  new f( 50,  96, 13, 16, 0, -4), // jump-left
                  new f(102,  96, 13, 16, 0, -4), new f(89, 96, 13, 16, 0, -4), new f(76, 96, 13, 16, 0, -4), new f(63, 96, 13, 16, 0, -4), // walk-left
-                 new f(  0, 112, 13, 16, 0, -4), // idle-right
                  new f( 65, 112, 13, 16, 0, -4), // jump-right
                  new f( 13, 112, 13, 16, 0, -4), new f(26, 112, 13, 16, 0, -4), new f(39, 112, 13, 16, 0, -4), new f(52, 112, 13, 16, 0, -4), // walk-right
-                 new f( 81, 112, 14, 16), new f(96, 112, 16, 16), // carrot
-                 new f(112, 115, 16,  4), new f(112, 124, 16, 4), new f(112, 119, 16, 4) // grass
+                 new f( 81, 112, 14, 16), new f(96, 112, 16, 16), // key
                 ];
 
 };
