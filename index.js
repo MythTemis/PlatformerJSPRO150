3bbbf37d13f3017661c29269675e5f097f9fc988
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-12-09 00:39:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/home", (req,res) => {
     res.sendFile(path.join(__dirname+'/home.html'));
 });
 
-app.get("/", (req,res) => {
+app.get("", (req,res) => {
     res.sendFile(path.join(__dirname+'/login.html'));
 });
 
