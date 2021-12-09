49cd46f5166e4a4e5bed3e600657723d2c4eed35
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2021-12-09 09:55:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -76,4 +76,7 @@ app.get('/world/:id', async (req,res) => {
 
 
 
-app.listen(process.env.PORT || 3000);
+const server = app.listen(process.env.PORT || 3000, () => {
+    const port = server.address().port;
+    console.log(`Express is working on port ${port}`);
+  });
